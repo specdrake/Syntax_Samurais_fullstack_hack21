52d010fae4efe11edca03c99843673dbec4c6746
aaradhyaberi07 <64523034+aaradhyaberi07@users.noreply.github.com>
2021-06-25 17:49:39 +0530

    admin sections updated

--- backend/locowin/user/admin.py
+++ backend/locowin/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User,Profile,Aadhar
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('username','email',)
+
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ('owner__username','name',)
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Profile,ProfileAdmin)
+admin.site.register(Aadhar)
